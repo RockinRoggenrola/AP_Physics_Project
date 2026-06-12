92258774ad5bf351fb86dfbfac170970f36ea4bd
Maya Agge <144492900+maa734@users.noreply.github.com>
2026-06-12 11:25:16 -0400

    Update Test.py

--- Test.py
+++ Test.py
@@ -479,23 +479,39 @@ def create_graph_curves():
 
 
 def reset_graphs():
+    # Fully delete old graph curves and graph boxes, then rebuild them from zero.
     global graph_curves
+    global current_graph, speed_graph, emf_graph, torque_graph
+    global ia_curve, ib_curve, ic_curve, speed_curve, sync_curve, emf_curve
+    global motor_torque_curve, resist_torque_curve, net_torque_curve
 
     for curve_object in graph_curves:
         if curve_object != 0:
-            curve_object.data = []
+            curve_object.delete()
 
-    current_graph.xmin = 0
-    current_graph.xmax = graph_window
+    graph_curves = []
 
-    speed_graph.xmin = 0
-    speed_graph.xmax = graph_window
+    if current_graph != 0:
+        current_graph.delete()
+    if speed_graph != 0:
+        speed_graph.delete()
+    if emf_graph != 0:
+        emf_graph.delete()
+    if torque_graph != 0:
+        torque_graph.delete()
 
-    emf_graph.xmin = 0
-    emf_graph.xmax = graph_window
+    ia_curve = 0
+    ib_curve = 0
+    ic_curve = 0
+    speed_curve = 0
+    sync_curve = 0
+    emf_curve = 0
+    motor_torque_curve = 0
+    resist_torque_curve = 0
+    net_torque_curve = 0
 
-    torque_graph.xmin = 0
-    torque_graph.xmax = graph_window
+    create_graph_objects()
+    create_graph_curves()
 
 
 def plot_graphs():
@@ -569,7 +585,7 @@ def reset_simulation():
     global running, voltage_rms, frequency, rotor_R, load_torque, damping, J, rotor_radius
     global animation_steps, t, theta, omega, plot_counter, live_visible, constants_visible
 
-    running = False
+    running = True
     voltage_rms = voltage_rms_default
     frequency = frequency_default
     rotor_R = rotor_R_default
@@ -584,7 +600,7 @@ def reset_simulation():
     omega = 0.0
     plot_counter = 0
 
-    run_button.text = "Run"
+    run_button.text = "Pause"
     voltage_slider.value = voltage_rms
     frequency_slider.value = frequency
     rotor_R_slider.value = rotor_R
@@ -609,9 +625,13 @@ def reset_simulation():
     constants_button.text = "Show Constants"
     live_text.text = ""
     constants_text.text = ""
+
     reset_view()
     reset_graphs()
     update_rotor_visuals()
+    plot_counter = 0
+    plot_graphs()
+
 
 # Controls and instructions
 scene.append_to_caption("\nControls\n")
